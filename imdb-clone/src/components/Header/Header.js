4e9a1ca1ae66e2c18f32324b17b894b3d00be2b8
Rishik Chakravarty <rishikrc3@gmail.com>
2024-01-06 21:14:31 +0530

    header_fixed

--- imdb-clone/src/components/Header/Header.js
+++ imdb-clone/src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
-import img from "./logo/logo.jpg";
+import img from "../logo/logo.jpg";
+import "./Header.css";
 const Header = () => {
   return (
     <div className="header">
@@ -7,7 +8,13 @@ const Header = () => {
         <img src={img} alt="logo-" />
       </div>
 
-      <div className="nav-items"></div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>New Movies</li>
+          <li>About Us</li>
+        </ul>
+      </div>
     </div>
   );
 };
