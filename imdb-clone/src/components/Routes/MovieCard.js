1ff89c9c249bea0587fb6b7fd9711f499b220969
Rishik Chakravarty <rishikrc3@gmail.com>
2024-01-10 02:37:14 +0530

    building_cards

--- imdb-clone/src/components/Routes/MovieCard.js
+++ imdb-clone/src/components/Routes/MovieCard.js
@@ -1,5 +1,31 @@
+import React from "react";
+import "./Routes.css";
+import { useState, useEffect, useParams } from "react";
+
 const MovieCard = () => {
-  return <div>HI</div>;
+  const [movieData, setMovieData] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.omdbapi.com/?apikey=66263dea&i=tt0050083#"
+    );
+    const jsonData = await data.json();
+    console.log(jsonData.Title);
+    setMovieData(jsonData);
+  };
+  if (movieData === null) return <>Loading</>;
+  return (
+    <>
+      <div className="img">
+        <img src={movieData.Poster} alt="hi" />
+        {movieData.Title}
+      </div>
+    </>
+  );
 };
 
 export default MovieCard;
