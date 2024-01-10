aaccfaee64c07aa480e3c5a4c3904d9e0027a2da
Rishik Chakravarty <rishikrc3@gmail.com>
2024-01-11 00:44:41 +0530

    setting_about

--- imdb-clone/src/components/Routes/User.js
+++ imdb-clone/src/components/Routes/User.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 
 const User = () => {
+  const [userData, setUserData] = useState([]);
+  useEffect(() => {
+    console.log("Hi");
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    console.log("Fetch Data called");
+    const data = await fetch("https://api.github.com/users/rishikrc3");
+    const json = await data.json();
+    setUserData(json);
+    console.log(json.name);
+  };
+  if (userData == null) return <>Loading</>;
   return (
     <>
-      <div className="user-card">Name:Rishik</div>
+      <div className="user-card">
+        <div className="user-img">
+          <img src={userData.avatar_url} alt="" />
+        </div>
+        <div className="user-info">
+          <h1>Hi my name is :{userData.name}</h1>
+          <h1>{userData.login}@gmail.com</h1>
+          <h2>
+            I made this website, absolutely enjoyed it , if you got an
+            improvements, drop me a mail{" "}
+          </h2>
+        </div>
+      </div>
     </>
   );
 };
