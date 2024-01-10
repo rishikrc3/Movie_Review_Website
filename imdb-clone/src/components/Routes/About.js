a89e31ab6963f5e4138d063d00b3f58ed344a0ba
Rishik Chakravarty <rishikrc3@gmail.com>
2024-01-10 21:55:25 +0530

    basic_about

--- imdb-clone/src/components/Routes/About.js
+++ imdb-clone/src/components/Routes/About.js
@@ -1,10 +1,11 @@
 import React from "react";
-
+import User from "./User";
 const About = () => {
   return (
     <>
       <div className="About Us">
         <h1>About</h1>
+        <User />
       </div>
     </>
   );
---
+++ imdb-clone/src/components/Routes/User.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const User = () => {
+  return (
+    <>
+      <div className="user-card">Name:Rishik</div>
+    </>
+  );
+};
+
+export default User;
