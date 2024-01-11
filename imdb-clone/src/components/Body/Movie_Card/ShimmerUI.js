37fd2fd8aa0a28c07ac99278a20f7d01b7d9d6e8
Rishik Chakravarty <rishikrc3@gmail.com>
2024-01-12 02:58:17 +0530

    .

--- imdb-clone/src/components/Body/Movie_Card/ShimmerUI.js
+++ imdb-clone/src/components/Body/Movie_Card/ShimmerUI.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ShimmerUI = () => {
   return (
-    <div className="Shimmer-UI w-48 h-64 bg-gradient-to-r from-gray-300 via-gray-200 to-gray-300 animate-pulse shadow-md rounded-md overflow-hidden">
+    <div className="Shimmer-UI w-80 h-80 bg-gradient-to-r from-gray-300 via-gray-200 to-gray-300 animate-pulse shadow-md rounded-md overflow-hidden">
       ShimmerUI
     </div>
   );
