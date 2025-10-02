dd3f36fd3df24adb588fac76380b233f0bbd1959
rishik <rishikrc3@gmail.com>
2025-10-02 02:14:18 +0100

    wip:added-movie-response-type

--- imdb-clone/src/services/types/movietypes.ts
+++ imdb-clone/src/services/types/movietypes.ts
@@ -5,3 +5,20 @@ export type MovieType = {
     Year : string;
     imdbID: string;
 }
+
+export type MovieDetailType = {
+  Title: string;
+  Year: string;
+  Poster: string;
+  Plot: string;
+  Genre: string;
+  Released: string;
+  Runtime: string;
+  Director: string;
+  Actors: string;
+  imdbID: string;
+  imdbRating: string;
+  Type: string;
+  Response: string;
+  
+};
